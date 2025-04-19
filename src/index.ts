e1238b43e2d990228df99a1f6fa6be3c32536913
Charles77K <charlesobiora16@gmail.com>
2025-04-19 18:54:55 +0100

    feat: add helmet and compression

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./db/db";
 import { globalErrorHandler } from "./utils/errorHandler";
-
+import helmet from "helmet";
+import compression from "compression";
 import adminRouter from "./routes/adminRoutes";
 import candidateRouter from "./routes/candidateRoutes";
 import examRouter from "./routes/examRoutes";
@@ -21,6 +22,8 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 connectDB();
 
